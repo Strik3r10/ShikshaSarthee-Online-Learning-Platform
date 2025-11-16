9ac23288db7efadbb49c16b38bce188e8b7a2e11
Owais Farooqui <134964368+Strik3r10@users.noreply.github.com>
2025-11-16 23:08:26 +0530

    Update 2

--- controllers/authController.js
+++
@@ -1,6 +0,0 @@
-// minor fixes in routes
-console.log('Commit #3: minor fixes in routes');
-// added few test endpoints
-console.log('Commit #5: added few test endpoints');
-// last one before push
-console.log('Commit #25: last one before push');
--- controllers/productController.js
+++
@@ -1,2 +0,0 @@
-// small code cleanup
-console.log('Commit #9: small code cleanup');
