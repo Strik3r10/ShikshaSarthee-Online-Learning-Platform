6980284d800d6f1f6601a7855fba44be3f3d290d
Owais Farooqui <134964368+Strik3r10@users.noreply.github.com>
2025-11-16 23:09:27 +0530

    Update 5

--- routes/authRoutes.js
+++
@@ -1,4 +0,0 @@
-// frontend setup
-console.log('Commit #6: frontend setup');
-// small styling changes
-console.log('Commit #14: small styling changes');
--- routes/productRoutes.js
+++
@@ -1,4 +0,0 @@
-// hooked up API calls
-console.log('Commit #13: hooked up API calls');
-// UI polish and tweaks
-console.log('Commit #20: UI polish and tweaks');
