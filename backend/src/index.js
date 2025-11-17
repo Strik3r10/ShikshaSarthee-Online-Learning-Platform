9724fc19ad246c718619297815d44b31fb2427f9
Owais Farooqui <134964368+Strik3r10@users.noreply.github.com>
2025-11-17 18:46:54 +0530

    new index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,19 @@
-import dotenv from "dotenv"
-import db from './database/db.js';
-import {app} from './app.js'
-dotenv.config({
-    path: './.env'
-})
+// src/index.js
+import dotenv from "dotenv";
+import db from "./database/db.js";
+import { app } from "./app.js";
+dotenv.config({ path: "./.env" });
 
 console.log(`${process.env.DB_NAME}`);
 
-
 db()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log(" mongodb connection failed !!! ", err);
-})
+  .then(() => {
+    const PORT = Number(process.env.PORT) || 8000;
+    app.listen(PORT, () => {
+      console.log(`⚙️ Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("mongodb connection failed !!!", err);
+    process.exit(1);
+  });
